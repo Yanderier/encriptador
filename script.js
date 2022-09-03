e99b4529a2b394cc40e963ea49a4b06d3c5aec7d
Yandy <112776060+Yanderier@users.noreply.github.com>
2022-09-03 14:17:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensaje = document.querySelector(".mensaje");
+const btnCopy = document.querySelector(".copiar");
+btnCopy.style.display = "none"
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensaje.value = textoEncriptado
+    mensaje.style.backgroundImage="none"
+    inputTexto.value = ""
+    btnCopy.style.display = "block"
+
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }                
+    }
+    return stringEncriptada;
+    
+}
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(inputTexto.value)
+    mensaje.value = textoEncriptado
+    inputTexto.value = ""  
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [ ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+function copiar() {
+    
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+    alert("Texto Copiado")
+}
